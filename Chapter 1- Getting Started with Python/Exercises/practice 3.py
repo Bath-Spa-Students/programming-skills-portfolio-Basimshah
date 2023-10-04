d36a35e2576b88360e9431aa9bd33837fbbba47d
Basimshah <145331879+Basimshah@users.noreply.github.com>
2023-10-04 22:50:01 +0400

    modified code (used 4 loop)

--- Chapter 1- Getting Started with Python/Exercises/practice 3.py
+++ Chapter 1- Getting Started with Python/Exercises/practice 3.py
@@ -1,1 +1,4 @@
-print("1.\n2.\n3.\n4.\n5.\n6.\n7.\n8.\n9.")
+#the program includes the use of four loop to print the numbers in the range 1 to 10
+for i in range(1, 10):
+    #\n is used to move to the next line
+    print(i, end='.\n')
