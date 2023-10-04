df6ef9a22d60001821340b281a1cb6381c34f583
Basimshah <145331879+Basimshah@users.noreply.github.com>
2023-10-04 23:09:14 +0400

    practice 4.py

---
+++ Chapter 1- Getting Started with Python/Exercises/practice 4.py
@@ -0,0 +1,6 @@
+print("Enter the value of height")
+height=int(input())
+print("Enter the value of base")
+base=int(input())
+area=0.5*base*height
+print("\nSince the height entered is", height, "and base entered is", base, "\n\nThe area of triangle is:",area)
