09b4ddf33cd482a3e0404db95c3dfb1548351949
Basimshah <145331879+Basimshah@users.noreply.github.com>
2023-10-13 22:36:57 +0400

    Exercise 2.py

---
+++ Chapter 2- Variables & Comments/Exercise 2.py
@@ -0,0 +1,8 @@
+#assigning quote a value
+quote = "It's not what you look at that matters, it's what you see."
+
+#assigning author a value
+author = "Henry David Thoreau"
+
+#printing the values of author and quote
+print(author + " once said, " + quote)
