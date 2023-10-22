0a488bd4e6e7f490dcdbadb5cddb8e92d1039203
Basimshah <145331879+Basimshah@users.noreply.github.com>
2023-10-22 23:24:14 +0400

    Exercise 4.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 4.py
@@ -0,0 +1,6 @@
+# initialized a list that contains the names of the guests invited for the dinner
+guests = ["Ahad Pervaize", "Huzaifa awan", "Moosa Kamran"]
+
+# used for loop to iterate through the list and print a common message for each guest with their names
+for guest in guests:
+    print(f"Dear {guest}, Join me for dinner, good food, and great company!")
