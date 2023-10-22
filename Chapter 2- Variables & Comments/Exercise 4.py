2bd7d4138a9738822b99af84fab39b5fed5242b1
Basimshah <145331879+Basimshah@users.noreply.github.com>
2023-10-22 16:13:14 +0400

    Exercise 4.py

---
+++ Chapter 2- Variables & Comments/Exercise 4.py
@@ -0,0 +1,8 @@
+# Assigning a variable fav_num to store the value of the favourite number
+fav_num = 7
+
+#Create a message using an f-string to include your favorite number
+message = f"My favorite number is {fav_num:}."
+
+# Print the message to generate favourite number
+print(message)
