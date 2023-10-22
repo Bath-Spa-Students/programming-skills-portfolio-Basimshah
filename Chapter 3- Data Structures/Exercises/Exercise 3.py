f3c923abab922eed8d61d4df349b41d0cc36b60d
Basimshah <145331879+Basimshah@users.noreply.github.com>
2023-10-22 23:23:36 +0400

    Exercise 3.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 3.py
@@ -0,0 +1,6 @@
+# initialized a list "transportation" with favorite transportation items
+transportation = ["Lexus IS 250 2018", "Nissan Maxima 2016", "Toyota Land Cruiser Prado", "Nissan Sentra 2015", "Nissan Pathfinder"]
+
+# used for loop to iterate throught the list and print statements about these items
+for item in transportation:
+    print(f"I would like to own a {item}.")
