68dcbf7b911213eaf457c16643b2c99651b497e5
Basimshah <145331879+Basimshah@users.noreply.github.com>
2023-10-22 15:56:46 +0400

    Exercise 3.py

---
+++ Chapter 2- Variables & Comments/Exercise 3.py
@@ -0,0 +1,14 @@
+#Assigning a variable to name
+name="\tMy name is Basim Shah\n"
+
+#Printing name
+print(name)
+
+#printing name using lstrip
+print(name.lstrip())
+
+#printing name using rstrip
+print(name.rstrip())
+
+#printing name using strip
+print(name.strip())
