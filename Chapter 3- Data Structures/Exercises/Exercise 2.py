577917582cbadd1421dd69ea9fea18905c89911e
Basimshah <145331879+Basimshah@users.noreply.github.com>
2023-10-22 23:22:51 +0400

    Exercise 2.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 2.py
@@ -0,0 +1,7 @@
+# initializing a list with the names of 5 friends
+names = ["Ahad", "Fahad", "Huzaifa", "Affan", "Moosa"]
+
+# using a for loop to print a message for each name in the list
+for name in names:
+    message = f"Hello, {name}! I hope you're having a great day."
+    print(message)
