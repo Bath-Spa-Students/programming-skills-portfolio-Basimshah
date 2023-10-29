d2d0ca63e06d25d97638274e58f728a12f2b9a82
Basimshah <145331879+Basimshah@users.noreply.github.com>
2023-10-29 23:22:30 +0400

    Exercise 3.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise 3.py
@@ -0,0 +1,34 @@
+#Assigning a value "Green" to variable "alien_color"
+alien_color="Green"
+
+#Using if else statement to match color and proceed
+if alien_color== "Green":
+    print("congratulations player! you just earned 5 points")
+elif alien_color== "Yellow":
+    print("congratulations player! you just earned 10 points")
+else:
+    print("congratulations player! you just earned 15 points")
+
+
+#Assigning a value "Green" to variable "alien_color"
+alien_color="Red"
+
+#Using if else statement to match color and proceed
+if alien_color== "Green":
+    print("congratulations player! you just earned 5 points")
+elif alien_color== "Yellow":
+    print("congratulations player! you just earned 10 points")
+else:
+    print("congratulations player! you just earned 15 points")
+
+
+#Assigning a value "Green" to variable "alien_color"
+alien_color="Yelow"
+
+#Using if else statement to match color and proceed
+if alien_color== "Yellow":
+    print("congratulations player! you just earned 5 points")
+elif alien_color== "Yellow":
+    print("congratulations player! you just earned 10 points")
+else:
+    print("congratulations player! you just earned 15 points")
