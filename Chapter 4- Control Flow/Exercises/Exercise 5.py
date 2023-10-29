fd6f5297f780e63d61cfdb682a2ed4f26b3fbdf3
Basimshah <145331879+Basimshah@users.noreply.github.com>
2023-10-29 23:24:35 +0400

    Exercise 5.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise 5.py
@@ -0,0 +1,16 @@
+Fav_fruit= ("Watermellon", "Mango", "Pomegranate")
+
+if "Watermellon" in Fav_fruit:
+    print("it seems you really like Watermellon")
+
+if "Orange" in Fav_fruit:
+    print("it seems you really like Orange")
+
+if "Mango" in Fav_fruit:
+    print("it seems you really like Mango")
+
+if "Apple" in Fav_fruit:
+    print("it seems you really like Apple")
+
+if "Pomegranate" in Fav_fruit:
+    print("it seems you really like Pomegranate")
