b63809e5fc8e15296da5b52dbb299cc981615bc8
Basimshah <145331879+Basimshah@users.noreply.github.com>
2023-11-14 14:05:57 +0400

    Exercise 1.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise 1.py
@@ -0,0 +1,13 @@
+#Assigning a value to each variable stored in dictionary "person"
+person = {
+    "1st_name": "Ahad",
+    "last_name": "Pervaize",
+    "age": 18,
+    "city": "Pakistan"
+}
+
+#Printing each value of variable in dictionary "person"
+print("First Name:", person["1st_name"])
+print("Last Name:", person["last_name"])
+print("Age:", person["age"])
+print("lives in:", person["city"])
