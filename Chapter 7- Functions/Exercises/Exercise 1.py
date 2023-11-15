5ede11cd958c4a96b6d45820e52289b5884a930e
Basimshah <145331879+Basimshah@users.noreply.github.com>
2023-11-15 18:58:45 +0400

    Exercise 1.py

---
+++ Chapter 7- Functions/Exercises/Exercise 1.py
@@ -0,0 +1,8 @@
+# Using def function for display_message
+def display_message():
+    """Printing a message about what I have been learning in this chapter."""
+    # Printing a message about what is being learned in this chapter
+    print("I am learning about functions in this chapter.")
+
+# Call the display_message function
+display_message()
