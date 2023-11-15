e9533eb4c07e11fac8a3ce41a573f34120a19db1
Basimshah <145331879+Basimshah@users.noreply.github.com>
2023-11-15 15:13:14 +0400

    Exercise 3.py

--- Chapter 6- Loops/Exercises/Exercise 3.py
+++ Chapter 6- Loops/Exercises/Exercise 3.py
@@ -1,2 +1,4 @@
+# Using Infinite loop to continuously print a message
 while True:
+    # Printing a message which indicating that the loop is in action
     print("Infinite loop in action! Press Ctrl-C to stop.")
