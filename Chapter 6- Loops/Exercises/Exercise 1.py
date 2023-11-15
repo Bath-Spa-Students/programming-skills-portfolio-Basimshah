91a23a5828fa7d65b7a8acd62fd3113b03f77353
Basimshah <145331879+Basimshah@users.noreply.github.com>
2023-11-15 15:11:51 +0400

    Exercise 1,py

--- Chapter 6- Loops/Exercises/Exercise 1.py
+++ Chapter 6- Loops/Exercises/Exercise 1.py
@@ -1,7 +1,11 @@
+# Using while loop to ask the user for pizza toppings
 while True:
+    # Asking the user to input the desired pizza topping and type 'quit' once they are finished
     pizza_topping = input("Please input the pizza topping you desire and type 'quit' once you are done: ")
     
+    # Checking if the user entered 'quit'; if true, break the loop
     if pizza_topping.lower() == "quit":
         break
     
-    print(f"You'll add {pizza_topping} to your pizza.")
+    # Printing the pizza topping that will be added
+    print(f"{pizza_topping} will be added to your pizza as toppings.")
